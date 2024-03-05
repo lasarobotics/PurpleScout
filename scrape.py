b339f7d32bf538a472048a30ecd03c6d5081fe5c
JasonRen003 <Jasren003@gmail.com>
2024-03-04 16:01:48 -0800

    scouting training database

--- scrape.py
+++ scrape.py
@@ -43,42 +43,43 @@ EVENT = '2024txwac'
 
 
 # """
-matchList = []
+# matchList = []
 
-matches = tba.event_matches(EVENT)
-for match in matches:
-    # Get only qualifying matches
-    if 'qm' in match.key:
+# matches = tba.event_matches(EVENT)
+# for match in matches:
+#     # Get only qualifying matches
+#     if 'qm' in match.key:
 
-        matchID = match.key
-        matchNum = int(match.key.split('_')[1][2:])
-        red1 = int(match.alliances['red']['team_keys'][0][3:])
-        red2 = int(match.alliances['red']['team_keys'][1][3:])
-        red3 = int(match.alliances['red']['team_keys'][2][3:])
-        blue1 = int(match.alliances['blue']['team_keys'][0][3:])
-        blue2 = int(match.alliances['blue']['team_keys'][1][3:])
-        blue3 = int(match.alliances['blue']['team_keys'][2][3:])
-        matchList.append([matchID, matchNum, red1, red2, red3, blue1, blue2, blue3])
+#         matchID = match.key
+#         matchNum = int(match.key.split('_')[1][2:])
+#         red1 = int(match.alliances['red']['team_keys'][0][3:])
+#         red2 = int(match.alliances['red']['team_keys'][1][3:])
+#         red3 = int(match.alliances['red']['team_keys'][2][3:])
+#         blue1 = int(match.alliances['blue']['team_keys'][0][3:])
+#         blue2 = int(match.alliances['blue']['team_keys'][1][3:])
+#         blue3 = int(match.alliances['blue']['team_keys'][2][3:])
+#         matchList.append([matchID, matchNum, red1, red2, red3, blue1, blue2, blue3])
 
-# sort matchList by matchNum
-matchList.sort(key=lambda x: x[1])
+# # sort matchList by matchNum
+# matchList.sort(key=lambda x: x[1])
 
-# write matchList to data/matchList.csv
-with open('data/matchList.csv', 'w', newline='') as f:
-    writer = csv.writer(f)
-    writer.writerow(["matchID", "matchNum", "red1", "red2", "red3", "blue1", "blue2", "blue3"])
-    for row in matchList:
-        writer.writerow(row)
-    f.close()
+# # write matchList to data/matchList.csv
+# with open('data/matchList.csv', 'w', newline='') as f:
+#     writer = csv.writer(f)
+#     writer.writerow(["matchID", "matchNum", "red1", "red2", "red3", "blue1", "blue2", "blue3"])
+#     for row in matchList:
+#         writer.writerow(row)
+#     f.close()
 
-teams = tba.event_teams(EVENT)
+# teams = tba.event_teams(EVENT)
+
 data = sqlite3.connect('data/scout.db').cursor().execute(
       "SELECT * FROM scoutData").fetchall()
 
 
 def UpdateSheets():
     creds = ServiceAccountCredentials.from_json_keyfile_name(
-        'robotics-scouting-414004-d07cb2963d15.json', [
+        'googlesheets.json', [
             'https://spreadsheets.google.com/feeds',
             'https://www.googleapis.com/auth/drive'
         ])
@@ -107,7 +108,7 @@ def drive_url(image_bytes, filename=""):
   }
   # Authenticate using the service account credentials
   credentials = service_account.Credentials.from_service_account_file(
-      'robotics-scouting-414004-d07cb2963d15.json',
+      'googlesheets.json',
       scopes=['https://www.googleapis.com/auth/drive'])
 
   # Create the Google Drive API service
