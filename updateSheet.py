b285fab9763df3c79647a7636cf281ee882131e8
JasonRen003 <Jasren003@gmail.com>
2024-03-25 16:04:04 -0600

    code cleanup, env

--- updateSheet.py
+++ updateSheet.py
@@ -1,5 +1,9 @@
 import sqlite3, requests, json
 
+### Update this link every time a new version of the Sheets script is created ###
+SCRIPT = "https://script.google.com/macros/s/AKfycbw4Yu2gGhH1C6fGABoUeqBhAATC0gWxPxEFro5NEvsO7FMLaa0StxlVSzpnAS6o7cU/exec"
+#################################################################################
+
 def get_data(min, max):
     # return the rows whose matchNum is within the range
 
@@ -18,7 +22,12 @@ def get_data(min, max):
 
     return rows
 
-data = get_data(58,61)
+print("Input the range of matches to send to the sheet")
+print("(same value for min and max to send only one match)")
+min = input("first match? ")
+max = input("last match? ")
+
+data = get_data(min,max)
 
 to_send = []
 for d in data:
@@ -34,8 +43,11 @@ for d in data:
     to_send[-1]["scoutID"] = d[3]
 
 print(to_send)
-print(f"Sending {len(to_send)} records")
-
-print(requests.post("https://script.google.com/macros/s/AKfycbw4Yu2gGhH1C6fGABoUeqBhAATC0gWxPxEFro5NEvsO7FMLaa0StxlVSzpnAS6o7cU/exec", data=json.dumps(to_send).encode()).content)
-
-# print(get_data(1, 100))
+if input(f"Sending {len(to_send)} records for {max-min+1} matches. Confirm? (y/n) ") == "y":
+    print("Sending... ", end="")
+    req = requests.post(SCRIPT, data=json.dumps(to_send).encode())
+    print("Done!")
+    print(req.content)
+else:
+    print("Aborted")
+    exit(0)
